b3717fd53607a831e80f326d4f9ddcbfbefb04dd
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-02-01 03:41:34 +0700

    :sparkles: Add push-screenshot command

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,9 @@ yargs
   .command('update-screenshots', 'Updates the screenshots.', {}, args =>
     require('./update-screenshots').run()
   )
+  .command('push-screenshots', 'Commits and pushs the screenshots.', {}, args =>
+    require('./push-screenshots').run()
+  )
   .demandCommand()
   .help()
   .strict()
