b5ed645900987466b2476dd7aa7aba2c463ae587
Phumrapee Limpianchop <git@rayriffy.com>
2019-10-02 10:18:40 +0700

    :art: Coming soon component

--- templates/coming-soon/components/index.tsx
+++ templates/coming-soon/components/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { Flex, Box, Image, Text, Heading } from 'rebass'
+import styled from 'styled-components'
+
+const FullHeightFlex = styled(Flex)`
+  height: 100% !important;
+`
+
 const ComingSoonComponent: React.FC = () => {
   return (
-    <>OK</>
+    <FullHeightFlex justifyContent={`center`} alignItems={`center`}>
+      <Box>
+        <Flex justifyContent={`center`}>
+          <Image width={[14 / 24, 12 / 24, 10 / 24, 8 / 24]} src={`/static/images/ycclogo.png`} />
+        </Flex>
+        <Box px={3} py={2}>
+          <Text textAlign={`center`} fontWeight={100} fontSize={52}>Young Creator's Camp</Text>
+          <Text textAlign={`center`} fontSize={28} pt={3}>ค่ายเปลี่ยนวัยมันส์เป็น “นักสร้างสรรค์” กำลังจะกลับมาเร็วๆ นี้</Text>
+        </Box>
+      </Box>
+    </FullHeightFlex>
   )
 }
 
