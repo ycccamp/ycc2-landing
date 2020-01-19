4f9a190036c4363266bc0986e04325b40ee18b36
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-19 13:02:25 +0700

    feat: preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
-module.exports = {
-  exportTrailingSlash: true,
+const withPreact = (nextConfig = {}) => {
+	return Object.assign({}, nextConfig, {
+		webpack(config, options) {
+			if (!options.defaultLoaders) {
+				throw new Error(
+					"This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+				)
+			}
+
+			if (options.isServer) {
+				config.externals = ["react", "react-dom", ...config.externals]
+			}
+
+			config.resolve.alias = Object.assign({}, config.resolve.alias, {
+				react: "preact/compat",
+				react$: "preact/compat",
+				"react-dom": "preact/compat",
+				"react-dom$": "preact/compat"
+			})
+
+			if (typeof nextConfig.webpack === "function") {
+				return nextConfig.webpack(config, options)
+			}
+
+			return config
+		}
+	})
 }
+
+module.exports = withPreact({
+  exportTrailingSlash: true,
+})
