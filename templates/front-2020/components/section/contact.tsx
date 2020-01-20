8c2fe68179191389185a650d1a57964c15d9bd6e
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-21 00:23:30 +0700

    refactor: link labels

--- templates/front-2020/components/section/contact.tsx
+++ templates/front-2020/components/section/contact.tsx
@@ -18,7 +18,10 @@ export const Contact: React.FC = props => {
         </Heading>
         <Flex alignItems='center' justifyContent='center' flexWrap='wrap'>
           <Box width={['100%', 1 / 2, 1 / 2, 1 / 3]} p={6}>
-            <Link href='https://facebook.com/ycccampth' isExternal>
+            <Link
+              href='https://facebook.com/ycccampth'
+              isExternal
+              aria-label='Facebook'>
               <AspectRatioBox ratio={1 / 1} bg='facebook.500' borderRadius={20}>
                 <Flex alignItems='center' justifyContent='center'>
                   <Box as={FaFacebookF} size='64px' color='white' />
@@ -27,7 +30,10 @@ export const Contact: React.FC = props => {
             </Link>
           </Box>
           <Box width={['100%', 1 / 2, 1 / 2, 1 / 3]} p={6}>
-            <Link href='https://twitter.com/ycccampth' isExternal>
+            <Link
+              href='https://twitter.com/ycccampth'
+              isExternal
+              aria-label='Twitter'>
               <AspectRatioBox ratio={1 / 1} bg='twitter.500' borderRadius={20}>
                 <Flex alignItems='center' justifyContent='center'>
                   <Box as={FaTwitter} size='64px' color='white' />
@@ -36,7 +42,10 @@ export const Contact: React.FC = props => {
             </Link>
           </Box>
           <Box width={['100%', 1 / 2, 1 / 2, 1 / 3]} p={6}>
-            <Link href='https://www.instagram.com/ycccampth' isExternal>
+            <Link
+              href='https://www.instagram.com/ycccampth'
+              isExternal
+              aria-label='Instagram'>
               <AspectRatioBox ratio={1 / 1} bg='pink.500' borderRadius={20}>
                 <Flex alignItems='center' justifyContent='center'>
                   <Box as={FaInstagram} size='64px' color='white' />
