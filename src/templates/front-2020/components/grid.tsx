35272ecb10538881a1437a2b3c336b9dad25fa36
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-23 01:04:00 +0700

    fix: grid size should be determined by window size

--- src/templates/front-2020/components/grid.tsx
+++ src/templates/front-2020/components/grid.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useViewportSize } from 'web-api-hooks'
+import { useWindowSize } from 'web-api-hooks'
 
 import { PseudoBox } from '@chakra-ui/core'
 
@@ -8,7 +8,7 @@ export const Grid: React.FC = props => {
   const size = 2
   const space = 50
 
-  const [viewportWidth, viewportHeight] = useViewportSize()
+  const [windowWidth, windowHeight] = useWindowSize()
 
   return (
     <PseudoBox
@@ -20,29 +20,28 @@ export const Grid: React.FC = props => {
       right={0}
       left={0}
       border={`${size}px solid transparent`}>
-      {Array.apply(
-        null,
-        Array(Math.floor(viewportHeight / (size + space)))
-      ).map((_, i) => (
-        <tr
-          key={`bg-table-row-${i}`}
-          style={{ border: `${size}px solid transparent` }}>
-          {Array.apply(
-            null,
-            Array(Math.floor(viewportWidth / (size + space)))
-          ).map((_, j) => (
-            <td
-              key={`bg-table-row-${i}-col-${j}`}
-              style={{
-                background: 'white',
-                backgroundClip: 'padding-box',
-                padding: `${space}px`,
-                border: `${size}px solid transparent`,
-              }}
-            />
-          ))}
-        </tr>
-      ))}
+      {Array.apply(null, Array(Math.floor(windowHeight / (size + space)))).map(
+        (_, i) => (
+          <tr
+            key={`bg-table-row-${i}`}
+            style={{ border: `${size}px solid transparent` }}>
+            {Array.apply(
+              null,
+              Array(Math.floor(windowWidth / (size + space)))
+            ).map((_, j) => (
+              <td
+                key={`bg-table-row-${i}-col-${j}`}
+                style={{
+                  background: 'white',
+                  backgroundClip: 'padding-box',
+                  padding: `${space}px`,
+                  border: `${size}px solid transparent`,
+                }}
+              />
+            ))}
+          </tr>
+        )
+      )}
     </PseudoBox>
   )
 }
