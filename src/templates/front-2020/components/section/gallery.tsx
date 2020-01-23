615f4fd8e16c58b15fb678345d47241d8a7877b5
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-23 13:26:33 +0700

    feat: optimized gallery

--- src/templates/front-2020/components/section/gallery.tsx
+++ src/templates/front-2020/components/section/gallery.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-import { AspectRatioBox, Box, Flex, Heading, Image } from '@chakra-ui/core'
+import { AspectRatioBox, Box, Flex, Heading } from '@chakra-ui/core'
 import { theme } from '../../theme'
 
 export const Gallery: React.FC = props => {
+  const mainImage = require('../../../../../assets/image/2020/gallery-1.jpg?resize&sizes[]=200&sizes[]=300sizes[]=400&sizes[]=600')
+  const mainImageWebP = require(`../../../../../assets/image/2020/gallery-1.jpg?webp`)
+
   return (
     <Flex
       justifyContent='center'
@@ -26,67 +29,58 @@ export const Gallery: React.FC = props => {
                 theme.shadows.xl,
                 theme.shadows['2xl'],
               ]}>
-              <Image
-                src={require('../../../../../assets/image/2020/gallery-1.jpg?resize&size=600')}
-                alt='Gallery Image 1'
-                objectFit='cover'
-              />
+              <Box objectFit='cover'>
+                <picture>
+                  <source srcSet={mainImageWebP} type='image/webp' />
+                  <source
+                    src={mainImage.src}
+                    srcSet={mainImage.srcSet}
+                    type='image/jpeg'
+                  />
+                  <img
+                    src={mainImage.src}
+                    srcSet={mainImage.srcSet}
+                    alt='Gallery Image 1'
+                  />
+                </picture>
+              </Box>
             </AspectRatioBox>
           </Box>
         </Flex>
         <Flex justifyContent='center' flexWrap='wrap'>
-          <Box width={['100%', 1 / 2, 1 / 2, 1 / 4]} p={2}>
-            <AspectRatioBox
-              ratio={16 / 9}
-              borderRadius={20}
-              overflow='hidden'
-              boxShadow={theme.shadows.xl}>
-              <Image
-                src={require('../../../../../assets/image/2020/gallery-2.jpg?resize&size=500')}
-                alt='Gallery Image 2'
-                objectFit='cover'
-              />
-            </AspectRatioBox>
-          </Box>
-          <Box width={['100%', 1 / 2, 1 / 2, 1 / 4]} p={2}>
-            <AspectRatioBox
-              ratio={16 / 9}
-              borderRadius={20}
-              overflow='hidden'
-              boxShadow={theme.shadows.xl}>
-              <Image
-                src={require('../../../../../assets/image/2020/gallery-3.jpg?resize&size=500')}
-                alt='Gallery Image 3'
-                objectFit='cover'
-              />
-            </AspectRatioBox>
-          </Box>
-          <Box width={['100%', 1 / 2, 1 / 2, 1 / 4]} p={2}>
-            <AspectRatioBox
-              ratio={16 / 9}
-              borderRadius={20}
-              overflow='hidden'
-              boxShadow={theme.shadows.xl}>
-              <Image
-                src={require('../../../../../assets/image/2020/gallery-4.jpg?resize&size=500')}
-                alt='Gallery Image 4'
-                objectFit='cover'
-              />
-            </AspectRatioBox>
-          </Box>
-          <Box width={['100%', 1 / 2, 1 / 2, 1 / 4]} p={2}>
-            <AspectRatioBox
-              ratio={16 / 9}
-              borderRadius={20}
-              overflow='hidden'
-              boxShadow={theme.shadows.xl}>
-              <Image
-                src={require('../../../../../assets/image/2020/gallery-5.jpg?resize&size=500')}
-                alt='Gallery Image 5'
-                objectFit='cover'
-              />
-            </AspectRatioBox>
-          </Box>
+          {[2, 3, 4, 5].map(o => {
+            const image = require(`../../../../../assets/image/2020/gallery-${o}.jpg?resize&sizes[]=200&sizes[]=300sizes[]=400&sizes[]=600`)
+            const webP = require(`../../../../../assets/image/2020/gallery-${o}.jpg?webp`)
+
+            return (
+              <Box
+                width={['100%', 1 / 2, 1 / 2, 1 / 4]}
+                p={2}
+                key={`gallery-${o}`}>
+                <AspectRatioBox
+                  ratio={16 / 9}
+                  borderRadius={20}
+                  overflow='hidden'
+                  boxShadow={theme.shadows.xl}>
+                  <Box objectFit='cover'>
+                    <picture>
+                      <source srcSet={webP} type='image/webp' />
+                      <source
+                        src={image.src}
+                        srcSet={image.srcSet}
+                        type='image/jpeg'
+                      />
+                      <img
+                        src={image.src}
+                        srcSet={image.srcSet}
+                        alt={`Gallery Image ${o}`}
+                      />
+                    </picture>
+                  </Box>
+                </AspectRatioBox>
+              </Box>
+            )
+          })}
         </Flex>
       </Box>
     </Flex>
