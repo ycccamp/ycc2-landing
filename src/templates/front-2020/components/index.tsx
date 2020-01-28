c3d6e4aa79af475474d76beacd21ddeab902e37f
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-28 19:56:03 +0700

    refactor: remove preview

--- src/templates/front-2020/components/index.tsx
+++ src/templates/front-2020/components/index.tsx
@@ -7,7 +7,6 @@ import { css, Global } from '@emotion/core'
 
 import { theme } from '../theme'
 
-import { Grid } from './grid'
 import { Navbar } from './navbar'
 import {
   About,
@@ -22,13 +21,9 @@ import {
   Venue,
 } from './section'
 
-import { usePreview } from '../../../core/services/usePreview'
-
 import smoothscroll from 'smoothscroll-polyfill'
 
 const FrontComponent: React.FC = props => {
-  const mode = usePreview()
-
   const size = 10
   const space = 150
 
@@ -53,47 +48,26 @@ const FrontComponent: React.FC = props => {
       </Head>
       <ThemeProvider theme={theme}>
         <CSSReset />
-        {mode === 1 ? (
-          <React.Fragment>
-            <Global
-              styles={css`
-                html {
-                  --scroll-behavior: smooth;
-                  scroll-behavior: smooth;
-                }
-                body {
-                  background: linear-gradient(
-                    270deg,
-                    rgba(244, 215, 242, 1) 0%,
-                    rgba(255, 255, 255, 1) 100%
-                  );
-                }
-              `}
-            />
-            <Grid />
-          </React.Fragment>
-        ) : (
-          <Global
-            styles={css`
-              html {
-                scroll-behavior: smooth;
-              }
-              body {
-                background-image: radial-gradient(
-                    rgba(64, 147, 164, 0.5) ${size}px,
-                    transparent ${size}px
-                  ),
-                  radial-gradient(
-                    rgba(232, 100, 119, 0.5) ${size}px,
-                    transparent ${size}px
-                  );
-                background-size: ${space * 2}px ${space * 2}px;
-                background-position: 0 0, ${space}px ${space}px;
-                animation: lineMove 2s infinite linear;
-              }
-            `}
-          />
-        )}
+        <Global
+          styles={css`
+            html {
+              scroll-behavior: smooth;
+            }
+            body {
+              background-image: radial-gradient(
+                  rgba(64, 147, 164, 0.5) ${size}px,
+                  transparent ${size}px
+                ),
+                radial-gradient(
+                  rgba(232, 100, 119, 0.5) ${size}px,
+                  transparent ${size}px
+                );
+              background-size: ${space * 2}px ${space * 2}px;
+              background-position: 0 0, ${space}px ${space}px;
+              animation: lineMove 2s infinite linear;
+            }
+          `}
+        />
         <Navbar />
         <Hero />
         <About />
