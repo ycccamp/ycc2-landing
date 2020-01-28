2ab0ef7bcc5a6c29d08f2aa38d6fffe796b1a89e
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-28 21:57:56 +0700

    feat: fb pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,21 @@ class NextApp extends App {
             property='twitter:image'
             content='https://ycc.in.th/static/og.jpg'
           />
+          {`<script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '970434653357485');
+            fbq('track', 'PageView');
+          </script>
+          <noscript><img height='1' width='1' style='display:none'
+            src='https://www.facebook.com/tr?id=970434653357485&ev=PageView&noscript=1'
+          /></noscript>`}
         </Head>
         <AppShell>
           <Component {...pageProps} />
