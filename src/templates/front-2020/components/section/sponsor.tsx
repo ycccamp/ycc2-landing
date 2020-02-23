6bf1b1195fed7ab40e96389833bf26ead9667cb0
Phumrapee Limpianchop <git@rayriffy.com>
2020-02-23 23:04:51 +0700

    feat: add sponsor finema, msp, qtft

--- src/templates/front-2020/components/section/sponsor.tsx
+++ src/templates/front-2020/components/section/sponsor.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { AspectRatioBox, Box, Flex, Heading, Image } from '@chakra-ui/core'
 
 export const Sponsor: React.FC = props => {
-  const sponsors = ['rise', 'svl', 'dekd']
+  const sponsors = ['rise', 'svl', 'dekd', 'finema', 'msp', 'qtft']
 
   return (
     <Flex
