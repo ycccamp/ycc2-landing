e47653fcb60858c2e0a007edb78cd55b24ed4195
Phumrapee Limpianchop <git@rayriffy.com>
2020-02-23 23:39:33 +0700

    feat: enable experimental flag

--- next.config.js
+++ next.config.js
@@ -1,50 +1,35 @@
+const withPlugins = require('next-compose-plugins')
+
 const optimizedImages = require('next-optimized-images')
 
-const withPreact = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      if (!options.defaultLoaders) {
-        throw new Error(
-          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
-        )
-      }
-
-      if (options.isServer) {
-        config.externals = ["react", "react-dom", ...config.externals]
-      }
-
-      config.resolve.alias = Object.assign({}, config.resolve.alias, {
-        react: "preact/compat",
-        react$: "preact/compat",
-        "react-dom": "preact/compat",
-        "react-dom$": "preact/compat"
-      })
-
-      if (typeof nextConfig.webpack === "function") {
-        return nextConfig.webpack(config, options)
-      }
-
-      return config
-    }
-  })
-}
-
-module.exports = optimizedImages(
-  withPreact({
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        optimizeImagesInDev: true,
+        mozjpeg: {
+          quality: 75,
+        },
+        optipng: {
+          optimizationLevel: 2,
+        },
+        webp: {
+          preset: 'default',
+          quality: 75,
+        },
+      },
+    ],
+  ],
+  {
     env: {
       FULLSTORY: process.env.FULLSTORY,
     },
     exportTrailingSlash: true,
-    optimizeImagesInDev: true,
-    mozjpeg: {
-      quality: 75,
+    experimental: {
+      reactMode: 'concurrent',
+      modern: true,
+      granularChunks: true,
     },
-    optipng: {
-      optimizationLevel: 2,
-    },
-    webp: {
-      preset: 'default',
-      quality: 75,
-    },
-  })
+  }
 )
