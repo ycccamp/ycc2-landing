03edf458d3ba6d2cb507596a21713ca61de0c862
Phoomparin Mano <phoomparin@gmail.com>
2020-03-16 00:29:52 +0700

    :bug: Fix print width

--- src/templates/front-2020/theme/index.ts
+++ src/templates/front-2020/theme/index.ts
@@ -1,4 +1,8 @@
-import { ColorHues, ITheme as _ITheme, theme as chakraTheme } from '@chakra-ui/core'
+import {
+  ColorHues,
+  ITheme as _ITheme,
+  theme as chakraTheme,
+} from '@chakra-ui/core'
 
 import colorLib from 'color'
 
@@ -52,8 +56,10 @@ export const theme: ITheme = {
   shadow: {
     sm: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
     md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-    lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-    xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    lg:
+      '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    xl:
+      '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
     '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
   },
 }
